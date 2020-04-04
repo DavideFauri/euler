57ec37d875b0a484ea420ddb66228596ed3e238d
Davide Fauri <davide.fauri@gmail.com>
2020-04-04 18:42:46 +0200

    003 (Haskell) - refactor

--- Haskell/Problem_003.hs
+++ Haskell/Problem_003.hs
@@ -6,5 +6,4 @@ factors n = filter (\p -> n `mod` p == 0) $ takeWhile (<= limit) primes
   where limit = floor . sqrt . fromIntegral $ n
 
 
-solution :: Integer
 solution = last . factors $ 600851475143
