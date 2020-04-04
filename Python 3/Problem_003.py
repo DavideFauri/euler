39ea1b5a22aaf4e53b98d3264d04cda7abe3d925
Davide Fauri <davide.fauri@gmail.com>
2020-04-04 18:40:30 +0200

    003 (py)

---
+++ Python 3/Problem_003.py
@@ -0,0 +1,19 @@
+# Find the largest prime factor of a composite number.
+# coding: utf-8
+
+from math import sqrt, floor
+from euler import primes
+
+
+def max_factor(N):
+    max_factor = None
+    limit = floor(sqrt(N))
+
+    for p in primes():
+        if N % p == 0:
+            max_factor = p
+        if p > limit:
+            return max_factor
+
+
+print(max_factor(600851475143))
