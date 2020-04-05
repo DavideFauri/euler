1f703d3850b6a8b552a9681c5638be40ae7666eb
Davide Fauri <davide.fauri@gmail.com>
2020-04-05 22:29:17 +0200

    004 (Haskell)

---
+++ Haskell/Problem_004.hs
@@ -0,0 +1,38 @@
+largestPalindrome :: Integer -> Integer
+largestPalindrome n =
+  head
+    $  filter (testIfFactorsHaveLength n)
+    $  (generatePalindromesOfLength $ 2 * n)
+    ++ (generatePalindromesOfLength $ 2 * n - 1)
+
+
+testIfFactorsHaveLength :: Integer -> Integer -> Bool
+testIfFactorsHaveLength length number = any (`hasLength` length)
+  $ map (number `div`) possibleDivisors
+ where
+  n `hasLength` l = 10 ^ (l - 1) <= abs n && abs n < 10 ^ l
+  possibleDivisors =
+    [ divisor | divisor <- (numbersOfLength length), number `mod` divisor == 0 ]
+
+
+numbersOfLength :: Integer -> [Integer]
+numbersOfLength digits = [maxNumber, maxNumber - 1 .. minNumber]
+ where
+  maxNumber = toInteger (10 ^ digits) - 1
+  minNumber = 10 ^ (fromIntegral digits - 1)
+
+
+generatePalindromesOfLength :: Integer -> [Integer]
+generatePalindromesOfLength length
+  | length `mod` 2 == 0 = map palyndromizeEven $ bases (length `div` 2)
+  | otherwise           = map palyndromizeOdd $ bases (length `div` 2 + 1)
+ where
+  palyndromizeEven n = read $ (show n) ++ (reverse $ show n)
+  palyndromizeOdd n = read $ (show n) ++ (tail $ reverse $ show n)
+  bases digits = [maxBase, maxBase - 1 .. minBase] -- 999 ... 100
+   where
+    maxBase = 10 ^ digits - 1
+    minBase = 10 ^ (digits - 1)
+
+
+solution = largestPalindrome 3
