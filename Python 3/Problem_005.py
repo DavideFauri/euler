305c81ddd8ea0b2276424c9f350a508ae0193886
Davide Fauri <davide.fauri@gmail.com>
2020-04-05 22:38:53 +0200

    005 (py)

---
+++ Python 3/Problem_005.py
@@ -0,0 +1,12 @@
+# What is the smallest number that is evenly divisible by all of the numbers from 1 to 20?
+from euler import lcm
+
+
+def divisible_by_all(N):
+    result = 1
+    for n in range(1, N + 1):
+        result = lcm(result, n)
+    return result
+
+
+print(divisible_by_all(20))
