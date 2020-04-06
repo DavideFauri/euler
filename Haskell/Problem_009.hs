2241f4f245d1a24cb749c4a4834924df73605f4b
Davide Fauri <davide.fauri@gmail.com>
2020-04-06 03:49:45 +0200

    009 (Haskell)

---
+++ Haskell/Problem_009.hs
@@ -0,0 +1,23 @@
+isPythagoreanTriplet :: (Integer, Integer, Integer) -> Bool
+isPythagoreanTriplet (a, b, c) = a ^ 2 + b ^ 2 == c ^ 2
+
+
+isProperTriangle :: (Integer, Integer, Integer) -> Bool
+isProperTriangle (a, b, c) = a < b + c && b < a + c && c < a + b
+
+
+triangleTripletsWithSum :: Integer -> [(Integer, Integer, Integer)]
+triangleTripletsWithSum n =
+  filter isPythagoreanTriplet $ filter isProperTriangle $ tripletsWithSum n
+
+
+tripletsWithSum :: Integer -> [(Integer, Integer, Integer)]
+tripletsWithSum n =
+  [ (a, b, n - a - b) | a <- [0 .. n `div` 2 + 1], b <- [0 .. n `div` 2] ]
+
+
+productTriplets :: [(Integer, Integer, Integer)] -> [Integer]
+productTriplets xs = map (\(a, b, c) -> a * b * c) xs
+
+
+solution = productTriplets $ triangleTripletsWithSum 1000
