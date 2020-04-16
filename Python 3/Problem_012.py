6a9f18641504918c57595264af8e6302e84e962d
Davide Fauri <davide.fauri@gmail.com>
2020-04-16 10:29:59 +0200

    012 (py)

---
+++ Python 3/Problem_012.py
@@ -0,0 +1,29 @@
+# What is the value of the first triangle number to have over five hundred divisors?
+from euler import factorize
+
+
+def triangle_numbers():
+    t = 0
+    i = 1
+    while True:
+        t += i
+        i += 1
+        yield t
+
+
+def n_divisors(n):
+    prime_factors = factorize(n)
+    counts = [prime_factors.count(p) for p in set(prime_factors)]
+    n_divisors = 1
+    for c in counts:
+        n_divisors *= c + 1
+    return n_divisors
+
+
+def triangle_with_n_divisors(limit):
+    for t in triangle_numbers():
+        if n_divisors(t) > limit:
+            return t
+
+
+print(triangle_with_n_divisors(500))
