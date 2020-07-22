8eeb3e5090d83876e60f84d4ccf7c9577a545109
Davide Fauri <davide.fauri@gmail.com>
2020-07-22 16:40:56 +0200

    Refactor Haskell library add more polymorphism to functions

--- Haskell/Euler.hs
+++ Haskell/Euler.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Euler
   ( fibonacci
   , factorial
@@ -8,22 +10,24 @@ module Euler
   )
 where
 import           Data.List                      ( nub )
-import           Data.Ratio                     ( (%) )
+import           Data.Ratio                     ( Ratio
+                                                , (%)
+                                                )
 
 
 -- canonical implementation of Fibonacci
-fibonacci :: [Integer]
+fibonacci :: Integral a => [a]
 fibonacci = 0 : 1 : zipWith (+) fibonacci (tail fibonacci)
 
 
 -- factorial
-factorial :: Integer -> Integer
+factorial :: Integral a => a -> a
 factorial 1 = 1
 factorial n = n * factorial (n - 1)
 
 
 -- primes generation
-primes :: [Integer]
+primes :: Integral a => [a]
 primes = 2 : 3 : spin wheel23 5 `minus` composites
  where
   spin (x : xs) n = n : spin xs (n + x)
@@ -41,27 +45,25 @@ primes = 2 : 3 : spin wheel23 5 `minus` composites
 
 
 -- factorization
-factorize :: Integer -> [Integer]
+factorize :: Integral a => a -> [a]
 factorize 1 = []
 factorize n = divisor : factorize (n `quot` divisor)
   where divisor = head $ filter (\p -> n `mod` p == 0) primes
 
 
 -- divisors of a number
-divisors :: Integer -> [Integer]
+divisors :: Integral a => a -> [a]
 divisors n =
-  let bound = floor . sqrt . fromInteger $ n
-  in  nub $ foldl (++)
-                  [1]
-                  [ [d, n `quot` d] | d <- [2 .. bound], n `mod` d == 0 ]
+  nub . foldl (++) [1] $ [ [d, n `quot` d] | d <- [2 .. bound], n `mod` d == 0 ]
+  where bound = floor . sqrt $ fromIntegral n
 
 
 -- n choose k
 -- https://stackoverflow.com/questions/28896626/tail-recursive-binomial-coefficient-function-in-haskell
-binomial :: Integer -> Integer -> Integer
-binomial = loop (toRational 1)
+binomial :: forall a . Integral a => a -> a -> a
+binomial = loop 1
  where
-  loop :: Rational -> Integer -> Integer -> Integer
+  loop :: Ratio a -> a -> a -> a
   loop r _ 0 = round r
   loop _ 0 _ = 0
   loop r n k = loop (r * (n % k)) (n - 1) (k - 1)
