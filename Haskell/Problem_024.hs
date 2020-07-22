8790d6dd0cf63bbd72dbd2e99fb2a78b1644580a
Davide Fauri <davide.fauri@gmail.com>
2020-07-22 16:38:57 +0200

    Refactor problem 24Type enforce of list being sorted

--- Haskell/Problem_024.hs
+++ Haskell/Problem_024.hs
@@ -1,26 +1,40 @@
 -- What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 import           Euler                          ( factorial )
-import           Data.List                      ( delete )
+import           Data.List                      ( permutations
+                                                , sort
+                                                )
+import           Data.SortedList                ( SortedList
+                                                , toSortedList
+                                                , fromSortedList
+                                                , delete
+                                                )
 
 
-nthPermutation :: Integer -> [Integer] -> Integer
-nthPermutation n elements = read . concat $ strPermutation
-  where strPermutation = [ show el | el <- nthPermutation' (n - 1) elements ]
+-- more complicated, but O(k) for k = #elements
+nthPermutation :: Int -> [Integer] -> Integer
+nthPermutation n elements =
+  read . concatMap show . nthPermutation' (n - 1) $ toSortedList elements
 
 
-nthPermutation' :: Integer -> [Integer] -> [Integer]
-nthPermutation' 0 el = el
-nthPermutation' _ [] = []
-nthPermutation' n elements =
-  rootSubtree : nthPermutation' nSubtree elementsSubtree
+-- use type SortedList to guarantee that input list is ascending
+nthPermutation' :: (Eq a, Ord a) => Int -> SortedList a -> [a]
+nthPermutation' 0 el = fromSortedList el
+nthPermutation' n elements
+  | elements == toSortedList [] = []
+  | otherwise = rootSubtree : nthPermutation' nSubtree elementsSubtree
  where
-  sizeOfSubtrees = factorial $ toInteger ( length elements) - 1
-  rootSubtree    = elements !! fromInteger ixRootSubtree
-    where ixRootSubtree = n `quot` sizeOfSubtrees
+  sizeOfSubtrees  = factorial (length elements - 1)
+  rootSubtree     = fromSortedList elements !! (n `quot` sizeOfSubtrees)
   nSubtree        = n `rem` sizeOfSubtrees
   elementsSubtree = delete rootSubtree elements
+
+
+-- simpler, but soooo much slower
+nthPermutation'' :: Int -> String -> String
+nthPermutation'' n digits = (sort . permutations $ digits) !! (n - 1)
 
 
 main :: IO ()
 main = do
   print $ nthPermutation 1000000 [0 .. 9]
+  print $ nthPermutation'' 1000000 "0123456789"
